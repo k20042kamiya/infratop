f17572eb6d7f775e01a938bbe10632de80ea3f36
k２００４２kk <k20042kk@kamiyamiyakoyuunoMacBook-Air.local>
2023-01-27 00:30:29 +0900

    課題提出

---
+++ omikuji.js
@@ -0,0 +1,32 @@
+let name;
+let username;
+let userresult;
+
+username = prompt("お名前を教えて下さい。");
+if (!username) {
+    document.getElementById("name").innerHTML = "名無し";
+}
+else {
+    document.getElementById("name").innerHTML = username;
+}
+
+
+let rand = Math.floor(Math.random() * 5);
+let omikuji = ["大吉", "中吉", "小吉", "吉", "凶"];
+userresult = omikuji[rand]
+// if (rand == 0) {
+//   userresult = "大吉";
+// }
+// if (rand == 1) {
+//   userresult = "中吉";
+// }
+// if (rand == 2) {
+//   userresult = "小吉";
+// }
+// if (rand == 3) {
+//   userresult = "吉";
+// }
+// if (rand == 4) {
+//   userresult = "凶";
+// }
+document.getElementById("result").innerHTML = userresult;
